e1f0a001df55bb30cd3a0e5f806f0409e90960b5
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-01-02 23:43:46 +0800

    react-folder-structure - components folder

---
+++ react-folder-structure-ref/src/components/index.ts
@@ -0,0 +1,9 @@
+// Common Components
+export { default as Header } from './common/Header';
+export { default as Footer } from './common/Footer';
+export { default as Layout } from './common/Layout';
+export { default as CarCard } from './common/CarCard';
+
+// Form Components
+export { default as TestDriveForm } from './forms/TestDriveForm';
+export { default as ContactForm } from './forms/ContactForm';
